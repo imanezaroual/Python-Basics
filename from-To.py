80c22b3567beb42a082faf729651154294aee01e
imanezaroual <imanezaroual59@gmail.com>
2024-11-26 20:50:21 +0100

    first commit

---
+++ from-To.py
@@ -0,0 +1,2 @@
+for m in range(1000 , 9555 ,10):
+    print(m)
---
+++ imane.py
@@ -0,0 +1,4 @@
+name= "imane zaroual"
+for m in range(len(name)):
+    print(name[m], end= "")
+print("\nprocess finished with exit code 0")
